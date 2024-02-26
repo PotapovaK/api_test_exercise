d7722325161879ef4a8aee6668f01a96c85ee958
PotapovaK <karinapotapova8@gmail.com>
2024-02-26 20:17:33 +0200

    "Work last week and today. Created tests for creating, updating, and getting a task"

---
+++ cypress/e2e/APITesting/DeleteTask.cy.js
@@ -0,0 +1,12 @@
+///<reference types='cypress'/>
+describe('Delete definite task', () => {
+    /*const randomNumber = Math.random().toString().slice(1);
+    let userId = randomNumber;*/
+    const requestBody = {
+        'content': 'string',
+        'user_id': 'string', ///userId,
+        'task_id': 'string',
+        'is_done': false  
+    }
+
+})
